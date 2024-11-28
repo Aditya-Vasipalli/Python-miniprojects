0770aa586e50299d29913c6d2daf4a0b58498ca6
Aditya Vasipalli <vasipalliaditya@gmail.com>
2024-11-28 14:35:43 +0530

    lab

--- lab 3/assignment10.py
+++ lab 3/assignment10.py
@@ -1,6 +1,5 @@
 #Given integer coordinates of three vertices of a rectangle whose sides are parallel to the coordinate axes, find the coordinates of the fourth vertex of the rectangle. In the first test the three given vertices are (1, 4), (1, 6), (7, 4). The fourth vertex is thus (7, 6).
 
-
 def missing_coord(coord1, coord2, coord3):
     if coord1==coord2==coord3:
         pass
