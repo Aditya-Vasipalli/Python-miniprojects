ab03f31ac87364625266f3474c7ff7409aaf35b9
Aditya Vasipalli <vasipalliaditya@gmail.com>
2024-12-01 16:46:59 +0530

    lab

--- lab 6/assignment30.py
+++ lab 6/assignment30.py
@@ -6,3 +6,11 @@ for i in range(10):
 #accessing List:
 index=int(input('enter the index you want to access: '))
 print('value is: ', list1[index])
+
+#accessing index:
+value=input('enter value to check')
+index=0
+for i in list1:
+    index+=1
+    if i==value:
+        print('found at index', index)
