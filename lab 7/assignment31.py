10add6c3c94ad55da852c55d4d761a7df665e380
Aditya Vasipalli <vasipalliaditya@gmail.com>
2024-12-01 17:03:38 +0530

    lab

---
+++ lab 7/assignment31.py
@@ -0,0 +1,9 @@
+#program to add 2 lists
+list1=[input('enter value for list 1: ' for i in range(10))]
+list2=[input('enter value for list 2: ' for i in range(10))]
+
+def add_list():
+    list3=list1+list2
+    return list3
+
+print(add_list())
