986b23f3cce8ade601f63525b6908f6a22ceaa29
Aditya Vasipalli <vasipalliaditya@gmail.com>
2024-12-01 17:03:18 +0530

    lab

--- lab 6/assignment30.py
+++ lab 6/assignment30.py
@@ -1,14 +1,12 @@
 #Program to access List Index and Values
 #first create list:
-list1=[]
-for i in range(10):
-    list1.append(input('enter value to add to list'))
+list1=[input('enter values in list' for i in range(10))]
 #accessing List:
 index=int(input('enter the index you want to access: '))
 print('value is: ', list1[index])
 
 #accessing index:
-value=input('enter value to check')
+value=input('enter value to check index of: ')
 index=0
 for i in list1:
     index+=1
