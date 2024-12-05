09c231596b2191eda91cd9f590226b6f64216cb6
Aditya Vasipalli <vasipalliaditya@gmail.com>
2024-12-05 19:36:07 +0530

    lab

--- lab 4/assignment15.py
+++ lab 4/assignment15.py
@@ -7,10 +7,14 @@ except ValueError:
 
 #equation = a*x**2+b*x+c
 def find_roots(a,b,c):
-    disc=(b**2-4*a*c)**(1/2)
-    print(disc)
-    root_1=(-b-disc)/(2*a)
-    root_2=(-b+disc)/(2*a)
-    return (root_1,root_2)
+    disc=(b**2-4*a*c)
+    root_1=(-b-disc**(1/2))/(2*a)
+    root_2=(-b+disc**(1/2))/(2*a)
+    if disc<0:
+        print('the roots are imaginary ', root_1, root_2)
+    elif disc>0:
+        print('the roots are real ', root_1, root_2)
+    else:
+        print('roots are equal')
 print(f'the equation is {a}x**2+ {b}x + {c}')
-print('the roots are', find_roots(a,b,c))
+find_roots(a,b,c)
